1a7de02f8e2d8ee2fbbf85c39163fd7018671817
Roberto Manfreda <robertomanfreda1994@gmail.com>
2020-06-09 12:00:13 +0200

    [ADD] - JAVA 11

---
+++ src/main/java/com/github/robertomanfreda/java11/Main.java
@@ -0,0 +1,11 @@
+package com.github.robertomanfreda.java11;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\n\n+-------------------------+");
+        System.out.println("| Java 11 - main features |");
+        System.out.println("+-------------------------+");
+        System.out.println("Di seguito una serie di esempi delle novità portate dalla versione 11 di Java.");
+    }
+}
